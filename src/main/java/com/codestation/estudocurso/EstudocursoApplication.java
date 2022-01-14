42a519b73472a3eb279475309ba0e0113b2b7f7e
Pitter Roberson <pitterroberson@gmail.com>
2022-01-13 23:07:03 -0300

    primeiro commit

---
+++ src/main/java/com/codestation/estudocurso/EstudocursoApplication.java
@@ -0,0 +1,13 @@
+package com.codestation.estudocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudocursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudocursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestation/estudocurso/EstudocursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestation.estudocurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudocursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
